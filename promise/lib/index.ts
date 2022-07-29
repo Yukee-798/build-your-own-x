196638448a29c704ddd9a0b6a91d0aca031d2f0c
yukee-798 <yukee798@gmail.com>
2022-07-29 21:18:03 +0800

    feat: 修改构造函数部分类型

--- promise/lib/index.ts
+++ promise/lib/index.ts
@@ -1,32 +1,42 @@
-class __Promise {
-  static PROMISE_STATUS_PENDING = 0;
-  static PROMISE_STATUS_FULFILLED = 1;
-  static PROMISE_STATUS_REJECTED = 2;
+export enum PromiseStatus {
+  PENDING = 0,
+  FULFILLED = 1,
+  REJECTED = 2,
+}
 
-  status = __Promise.PROMISE_STATUS_PENDING;
-  value: any;
-  reason: any;
+export type PromiseResolve<T> = (value?: T) => void;
+export type PromiseReject = (reason?: Error) => void;
+
+class __Promise<T> {
+  status: PromiseStatus = PromiseStatus.PENDING;
+  value: T | undefined;
+  reason: Error | undefined;
 
   // 通过 then、catch 绑定的多个微任务会放在下面的队列中
-  onFulfilledTasks = [];
-  onRejectedTasks = [];
+  private onFulfilledTasks: Array<(value?: T) => void> = [];
+  private onRejectedTasks: Array<(reason?: Error) => void> = [];
 
-  constructor(executor) {
-    const resolve = (value) => {
-      if (this.status === __Promise.PROMISE_STATUS_PENDING) {
-        this.status = __Promise.PROMISE_STATUS_FULFILLED;
+  constructor(
+    executor: (resolve: PromiseResolve<T>, reject: PromiseReject) => any
+  ) {
+    const resolve: PromiseResolve<T> = (value) => {
+      if (this.status === PromiseStatus.PENDING) {
+        this.status = PromiseStatus.FULFILLED;
         this.value = value;
         while (this.onFulfilledTasks.length > 0) {
-          const task = this.onFulfilledTasks.shift();
+          const task = this.onFulfilledTasks.shift()!;
+          task(value);
         }
       }
     };
-    const reject = (reason) => {
-      if (this.status === __Promise.PROMISE_STATUS_PENDING) {
-        this.status = __Promise.PROMISE_STATUS_REJECTED;
+
+    const reject: PromiseReject = (reason) => {
+      if (this.status === PromiseStatus.PENDING) {
+        this.status = PromiseStatus.REJECTED;
         this.reason = reason;
         while (this.onRejectedTasks.length > 0) {
-          // this.onRejectedTasks.shift()(reason);
+          const task = this.onRejectedTasks.shift()!;
+          task(reason);
         }
       }
     };
@@ -39,4 +49,15 @@ class __Promise {
   }
 }
 
-module.exports = __Promise;
+const p = new __Promise<{
+  code: number;
+  data: {
+    list: {
+      id: number;
+      name: string;
+      age: number;
+    }[];
+  };
+}>((resolve, reject) => {});
+
+export default __Promise;
